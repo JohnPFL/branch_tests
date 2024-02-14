bc36482a406f50bda94d30dc6afd268af94e6dae
JohnPFL <ytdossier@gmail.com>
2024-02-14 12:38:18 +0100

    first features added. Main created

---
+++ test.py
@@ -0,0 +1,21 @@
+# pylint: disable=trailing-whitespace
+
+"""This class will print artists, albums and songs!"""
+class ArtistPrinter:
+    """This class will print artists, albums and songs!"""
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def print_artist(artist_dict: dict):
+        """This function will print artists in the dictionary!"""
+        for artist in artist_dict.keys():
+            print(artist)
+
+
+if __name__ == '__main__':
+    # input variable
+    artists = {'Aphex Twin':
+           {'Album': {'Selected works': ['Xtal', 'Tha', 'Pulsewidth', 'Ageispolis']
+                      ,'Selected works II': []}}}
+    ArtistPrinter().print_artist(artists)
